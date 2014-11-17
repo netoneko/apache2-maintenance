0563b18c26077dd58c284573c4d1cebd6d3d74e0
Kirill Maksimov <kirill@jammer.fm>
2014-11-17 16:07:21 +0200

    Add template and default clock

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+node[:apache2][:maintenance][:period] = 15
+node[:apache2][:maintenance][:unit] = 'minutes'
+
--- libraries/recipe.rb
+++ libraries/recipe.rb
@@ -3,5 +3,16 @@ class Chef
     def maintenance_page
       "#{node[:apache][:document_root]}/shared/system/maintenance.html"
     end
+
+    def maintenance_start
+      Time.now
+    end
+
+    def maintenance_stop
+      period = node[:apache2][:maintenance][:period]
+      unit = node[:apache2][:maintenance][:unit]
+
+      period.send(unit).from_now
+    end
   end
 end
