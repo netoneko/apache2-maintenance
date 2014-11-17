1c149cdeb88f9b8de7fdb86eed4952383312475e
Kirill Maksimov <kirill@jammer.fm>
2014-11-17 16:21:23 +0200

    Fix time format

--- libraries/recipe.rb
+++ libraries/recipe.rb
@@ -1,18 +1,20 @@
 class Chef
   class Recipe
+    MAINTENANCE_TIME_FORMAT = '%Y-%m-%dT%H:%M:%S%:z'
+
     def maintenance_page
       "#{node[:apache][:document_root]}/shared/system/maintenance.html"
     end
 
     def maintenance_start
-      Time.now
+      Time.now.strftime(MAINTENANCE_TIME_FORMAT)
     end
 
     def maintenance_stop
       period = node[:apache2][:maintenance][:period]
       unit = node[:apache2][:maintenance][:unit]
 
-      period.send(unit).from_now
+      period.send(unit).from_now.strftime(MAINTENANCE_TIME_FORMAT)
     end
   end
 end
