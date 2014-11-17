8efa58fa7e6138a876ca613a0daa8bbd79b7df5b
Kirill Maksimov <kirill@jammer.fm>
2014-11-17 15:39:41 +0200

    Add template and change location

--- libraries/recipe.rb
+++ libraries/recipe.rb
@@ -1,7 +1,7 @@
 class Chef
   class Recipe
     def maintenance_page
-      "#{node[:apache][:document_root]}/system/maintenance.html"
+      "#{node[:apache][:document_root]}/shared/system/maintenance.html"
     end
   end
 end
